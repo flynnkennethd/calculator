b45a02e5d61151a75e67445132aba3c4a90e0d87
Kenneth Flynn <flynnkennethd@gmail.com>
2021-11-22 23:45:22 -0500

    fully functional calculator. Finished display, finished functions for clearing, solving, deleting, finding operator and all that other stuff.

--- script.js
+++ script.js
@@ -11,41 +11,97 @@ class Calculator {
     constructor(prevNumDiv, currentNumDiv) {
         this.prevNumDiv = prevNumDiv;
         this.currentNumDiv = currentNumDiv;
+        this.clear();
     }
 
     clear() {
         this.prevNum = '';
         this.currentNum = '';
-        this.operation = undefined;
+        this.operator = undefined;
     }
 
     delete() {
-
+        this.currentNum = this.currentNum.slice(0, -1);
     }
 
     appendNum(number) {
-        this.currentNum = number;
+        if(number === '.' && this.currentNum.includes('.')) return;
+        this.currentNum = this.currentNum.toString() + number.toString();
     }
 
     chooseOperator(operator) {
-
+        if(!this.currentNum) return;
+        if(this.prevNum !== '') {
+            this.solve();
+        }
+        this.operator = operator;
+        this.prevNum = this.currentNum;
+        this.currentNum = '';
     }
 
     solve() {
-
+        let answer;
+        const prev = parseFloat(this.prevNum);
+        const current = parseFloat(this.currentNum);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch(this.operator) {
+            case '+':
+                answer = prev + current;
+                break;
+            case '-':
+                answer = prev - current;
+                break;
+            case 'x':
+                answer = prev * current;
+                break;
+            case '÷':
+                answer = prev / current;
+                break;
+            default:
+                return;
+        }
+        this.currentNum = answer;
+        this.operator = undefined;
+        this.prevNum = '';
     }
-
+    
     updateDisplay() {
-        this.currentNumDiv.innerText = this.currentNum
+        this.currentNumDiv.innerText = this.this.currentNum;
+        if(this.operator != null) {
+            this.prevNumDiv.innerText = 
+            `${this.prevNum} ${this.operator}`
+        }
+        else this.prevNumDiv.innerText = this.prevNum;
     }
 }
 
 const calculator = new Calculator(prevNumDiv, currentNumDiv);
 
-numBtns.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.appendNum(button.innerText);
+numBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        calculator.appendNum(btn.innerText);
         calculator.updateDisplay();
-        console.log('eventlistenerworked');
     })
+});
+
+operatorBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        calculator.chooseOperator(btn.innerText);
+        calculator.updateDisplay();
+    })
+});
+
+equalBtn.addEventListener('click', button => {
+    calculator.solve();
+    calculator.updateDisplay();
+});
+
+clearBtn.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+deleteBtn.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
 });
